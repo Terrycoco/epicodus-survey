2aafe518cd29e0c2c7649789889fdc31e2f0d0f7
Terry Marr & Tony Navarro <student@epicodus.com>
2015-08-25 10:45:29 -0700

    require input for question and survey to validate

--- lib/question.rb
+++ lib/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to(:surveys)
   has_many(:responses)
+  validates(:question, {presence: :true})
 end
--- lib/survey.rb
+++ lib/survey.rb
@@ -1,7 +1,8 @@
 class Survey < ActiveRecord::Base
   has_many(:questions)
+  validates(:title, {presence: :true})
   before_save(:title_upcase)
-  
+
 private
    define_method (:title_upcase) do
     self.title = title.upcase()
